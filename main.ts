ded2a89b68b8c3a9ee06df56ddbef4ffb0607966
Jamal Beatmaker <89939812+JamalBeatmaker@users.noreply.github.com>
2023-06-14 23:33:03 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+function Verificando_Verde () {
+    if (SENColor.getColor(color.green) > SENColor.getColor(color.red) && SENColor.getColor(color.green) > SENColor.getColor(color.blue)) {
+    	
+    } else if (false) {
+    	
+    } else {
+        hackbitmotors.MotorStop(hackbitmotors.Motors.M1A)
+    }
+}
 input.onButtonPressed(Button.A, function () {
     SENColor.calibrate()
+})
+input.onButtonPressed(Button.B, function () {
+    hackbitmotors.Servo(hackbitmotors.Servos.S8, 25)
 })
 led.enable(false)
 SENColor.setPins(
@@ -14,9 +26,11 @@ basic.forever(function () {
     serial.writeValue("G", SENColor.getColor(color.green))
     serial.writeValue("B", SENColor.getColor(color.blue))
     basic.pause(1000)
-    if (SENColor.getColor(color.green) > SENColor.getColor(color.red) && SENColor.getColor(color.green) > SENColor.getColor(color.blue)) {
-        hackbitmotors.MotorRunDelay(hackbitmotors.Motors.M1A, 150, 1)
-    } else {
-        hackbitmotors.MotorStop(hackbitmotors.Motors.M1A)
+    if (hackbitsensor.detectline(DigitalPin.P4, true) && hackbitsensor.detectline(DigitalPin.P10, true)) {
+        hackbitmotors.MotorStopAll()
+        basic.pause(100)
+        hackbitmotors.MotorRunDelay(hackbitmotors.Motors.M1A, -30, 1)
+        hackbitmotors.MotorRunDelay(hackbitmotors.Motors.M1B, -30, 1)
+        hackbitmotors.MotorStopAll()
     }
 })
